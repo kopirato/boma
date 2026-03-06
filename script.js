bf38bfb03509d61d7c9095bb07f1e05d60940e31
Selian.T <84496338+kopirato@users.noreply.github.com>
2026-03-06 20:15:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,40 +2,55 @@ let animals = JSON.parse(localStorage.getItem("animals")) || [];
 
 function addAnimal(){
 
-let animal={
+let file = document.getElementById("imageUpload").files[0];
 
-name:document.getElementById("name").value,
+let reader = new FileReader();
 
-age:document.getElementById("age").value,
+reader.onload = function(){
 
-price:document.getElementById("price").value,
+let animal = {
 
-location:document.getElementById("location").value,
+name: document.getElementById("name").value,
 
-phone:document.getElementById("phone").value,
+age: document.getElementById("age").value,
 
-image:document.getElementById("image").value
+price: document.getElementById("price").value,
+
+location: document.getElementById("location").value,
+
+phone: document.getElementById("phone").value,
+
+image: reader.result
 
 };
 
 animals.push(animal);
 
-localStorage.setItem("animals",JSON.stringify(animals));
+localStorage.setItem("animals", JSON.stringify(animals));
 
 displayAnimals();
+
+};
+
+if(file){
+
+reader.readAsDataURL(file);
+
+}
 
 }
 
 
+
 function displayAnimals(){
 
-let market=document.getElementById("market");
+let market = document.getElementById("market");
 
-market.innerHTML="";
+market.innerHTML = "";
 
-animals.forEach(a=>{
+animals.forEach(a => {
 
-market.innerHTML+=`
+market.innerHTML += `
 
 <div class="card">
 
@@ -49,7 +64,7 @@ market.innerHTML+=`
 
 <p>Location: ${a.location}</p>
 
-<a href="https://wa.me/${a.phone}">
+<a href="https://wa.me/${a.phone}" target="_blank">
 
 <button>Contact Seller</button>
 
